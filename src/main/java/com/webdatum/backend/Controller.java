a7bce26843a552a08af51a46f80c74caa69444fe
Benjamin Smith <webdatumconsulting@gmail.com>
2025-06-17 14:27:13 -0700

    Initial end points

---
+++ src/main/java/com/webdatum/backend/Controller.java
@@ -0,0 +1,18 @@
+package com.webdatum.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api/hello-world")
+    public String getHelloWorld(){
+        return "HelloWorld";
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "Home";
+    }
+}
